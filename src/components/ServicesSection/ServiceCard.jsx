3f74b99c28d2259ac4b65865ca33249cfc77cc0b
Efekan EFE <efekanefos@gmail.com>
2024-05-17 18:06:51 +0300

    day_2

--- src/components/ServicesSection/ServiceCard.jsx
+++ src/components/ServicesSection/ServiceCard.jsx
@@ -1,13 +1,23 @@
 import React from "react";
+import cardDecore from "../../assets/servicesImages/active_image.png";
 
 function ServiceCard({ card }) {
   return (
-    <div className="servicesCard">
-      <div className="servicesCardImgContent">
-        <img src={card.image} alt={card.title} className="servicesCardImg" />
+    <div className="servicesCardWrapper">
+      <div className="servicesCard">
+        <div className="servicesCardImgContent">
+          <img src={card.image} alt={card.title} className="servicesCardImg" />
+        </div>
+        <h3 className="serviceTitle">{card.title}</h3>
+        <p className="serviceCardDesc">{card.desc}</p>
       </div>
-      <h3 className="serviceTitle">{card.title}</h3>
-      <p className="serviceCardDesc">{card.desc}</p>
+      <div className="servicesCardImgContent--decore">
+        <img
+          src={cardDecore}
+          alt={card.title}
+          className="servicesCardImg--decore"
+        />
+      </div>
     </div>
   );
 }
