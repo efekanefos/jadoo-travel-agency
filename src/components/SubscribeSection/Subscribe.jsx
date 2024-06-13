aa1286e02c177659ecc0c917e2f69c75f33cb3d0
Efekan EFE <efekanefos@gmail.com>
2024-06-13 10:38:18 +0300

    subscribe

--- src/components/SubscribeSection/Subscribe.jsx
+++ src/components/SubscribeSection/Subscribe.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import sendingArrow from "../../assets/subscribeImages/sending_arrow.png";
-import leftEllips from "../../assets/subscribeImages/left_ellips.png";
-import rightEllips from "../../assets/subscribeImages/right_ellips.png";
 import "./Subscribe.css";
 
 function Subscribe() {
@@ -11,19 +9,14 @@ function Subscribe() {
         <img src={sendingArrow} alt="Purple Arrow" className="subscribeImg" />
       </div>
       <div className="subscribeContent">
-        <h2 className="subscribeTitle">
-          Subscribe to get information, latest news and other interesting offers
-          about Jadoo
-        </h2>
-
-        <div className="subscribeInputContent">
-          <input
-            type="text"
-            name="subscribe"
-            id="subscribe"
-            className="subscribeInput"
-            placeholder="Your email"
-          />
+        <div className="subscribeLeftEllips"></div>
+        <div className="subscribeRightEllips"></div>
+        <h2 className="subscribeTitle">Subscribe to get information, latest news and other interesting offers about Jadoo</h2>
+        <div className="subscribeSearchContent">
+          <div className="subscribeInputContent">
+            <input type="text" name="subscribe" id="subscribe" className="subscribeInput" placeholder="Your email" />
+          </div>
+          <input className="subscribeButton" type="submit" value="Subscribe" />
         </div>
       </div>
     </section>
